03dde612547952e8efc7cb19bd7975e22e9d895f
SAOUCHA <3670149@ppti-24-304-01.ufr-info-p6.jussieu.fr>
2019-03-27 12:22:20 +0100

    tme8

--- tme8_2.sql
+++ tme8_2.sql
@@ -1,6 +1,3 @@
---TD 8 - EXERCICE 1
--- question 1
-
 @vider
 
 CREATE TABLE Employe(
@@ -19,7 +16,7 @@ CREATE TABLE Employe(
 	CONSTRAINT nom_prenom_unique UNIQUE (NomE,PrenomE)
 );
 
-CREATE TABLE Projet(
+CREATE TABLE Projett(
 	NumProj number(7),
 	NomProj varchar2(20),
 	RespProj number(5) NOT NULL,
@@ -28,16 +25,14 @@ CREATE TABLE Projet(
 
 	CONSTRAINT Pk_proj PRIMARY KEY (NumProj),
 	CONSTRAINT Fk_proj_resp FOREIGN KEY (RespProj) references employe,
-	CONSTRAINT num_Proj CHECK (length(NumProj)>=5 and length(numProj)>=7),
-	CONSTRAINT ville_proj CHECK (lower(VilleP) IN ('Paris','Lyon','Marseille')));
-
-CREATE ASSERTION Ville_Proj_Resp(
-	CHECK (NOT EXISTS (SELECT * FROM Employe e, Projet p WHERE P.RespProj = e.NumSS and P.Ville <> e.VilleE));
+	CONSTRAINT num_Proj CHECK (length(NumProj)>=5 and length(numProj)<=7),
+	CONSTRAINT ville_proj CHECK (lower(VilleP) IN ('paris','lyon','marseille'))
+);
 
 
 CREATE TABLE GRILLE_SAL(
        Profil varchar2(20),
-       salaire roundnumber(5,2),
+       salaire number(5,2),
        CONSTRAINT pk_grille_sal primary key (profil),
        CONSTRAINT sal CHECK (salaire<90000 )
        );
@@ -46,15 +41,15 @@ CREATE TABLE EMBAUCHE(
        numSS number(5),
        NumProj number(7),
        DateEmb date,
-       Profil varchar2(20);
-       constraint pk_numss_embauche primary key (numss),
-       constraint pk_numproj_embauche primary key(numproj),
+       Profil varchar2(20),
+       constraint pk_embauche primary key (numss,numproj),
        constraint fk_embauche foreign key (numss) references employe,
-       constraint fk_numproj_embauche foreign key(numproj)references projet,
+       constraint fk_numproj_embauche foreign key(numproj)references projett,
        constraint fk_profil_embauche foreign key (profil) references grille_sal
        );
 
 SELECT table_name FROM user_tables;
        
-INSERT INTO Employe VALUES('21456','LARS', 'Anna','34567', 'paris', to_date('25-08- 1975'), sysdate);
+INSERT INTO Employe VALUES('21456','LARS', 'Anna',null, 'paris', to_date('25-08- 1975'), sysdate);
 --INSERT INTO Projet VALUES('3670404','TMESQL','21457','Paris','100000');
+
